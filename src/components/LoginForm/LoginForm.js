7f51aec8082f30c7f7d69ae61512943935497dec
Chris Halley <chrishalley86@gmail.com>
2019-02-07 17:22:22 +0000

    added input handler to Fieldset component

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -5,12 +5,20 @@ import { Fieldset, Button } from '../common';
 import styles from './LoginForm.module.css';
 
 class LoginForm extends Component {
+  state = { email: '', password: '' };
+
+  handleInput = (stateProp, value) => {
+    this.setState({ [stateProp]: value })
+  }
+
   render() {
     return (
       <div className={styles['login-form']}>
         <h3 className={styles['title']}>Login</h3>
-        <Fieldset label="Email" type="text" placeholder="eg. john@example.com"></Fieldset>
-        <Fieldset label="Password" type="text" placeholder="eg. password123"></Fieldset>
+        <Fieldset onInput={this.handleInput} stateProp="email" label="Email" type="text" placeholder="eg. john@example.com"></Fieldset>
+        <p>{this.state.email}</p>
+        <Fieldset onInput={this.handleInput} stateProp="password" label="Password" type="text" placeholder="eg. password123"></Fieldset>
+        <p>{this.state.password}</p>
         <Button>Log in</Button>
       </div>
     )
--- src/components/common/Fieldset/Fieldset.js
+++ src/components/common/Fieldset/Fieldset.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import styles from './Fieldset.module.css';
 
-const Fieldset = ({ label, type, errorMessage, placeholder }) => {
+const Fieldset = ({ label, type, stateProp, errorMessage, placeholder, onInput }) => {
+
   return (
     <React.Fragment>
       <fieldset className={styles['fieldset']}>
         <label htmlFor="">{label}</label>
-        <input type={type} placeholder={placeholder}/>
+        <input onChange={(e) => onInput(stateProp, e.target.value)} type={type} placeholder={placeholder}/>
         <div>{errorMessage}</div>
       </fieldset>
     </React.Fragment>
