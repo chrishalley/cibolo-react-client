e15fa0855339d8ba632993b4d5adbc45378928de
Chris Halley <chrishalley86@gmail.com>
2019-02-08 17:37:35 +0000

    added Toast component to Fieldset component as error handling display

--- src/components/common/Fieldset/Fieldset.js
+++ src/components/common/Fieldset/Fieldset.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
+import { Toast } from '../'
+
 import styles from './Fieldset.module.css';
 
 const Fieldset = ({ label, type, stateProp, errorMessage, placeholder, onChangeHandler }) => {
+
+  const renderToast = () => {
+    if (errorMessage) {
+      return (
+        <Toast type="error" content={errorMessage}/>
+      )
+    }
+  }
 
   return (
     <fieldset className={styles['fieldset']}>
       <label htmlFor="">{label}</label>
       <input onChange={(e) => onChangeHandler(stateProp, e.target.value)} type={type} placeholder={placeholder}/>
-      <div>{errorMessage}</div>
+      {renderToast()}
     </fieldset>
   )
 }
