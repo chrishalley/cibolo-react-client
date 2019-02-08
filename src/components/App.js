9c4adb07805d965ccb07743cb6cd4c8732e0f230
Chris Halley <chrishalley86@gmail.com>
2019-02-08 17:48:04 +0000

    added react-router-dom package and set up BrowserRouter inside App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter } from 'react-router-dom'
 
 import Header from './Header/Header'
 import LoginForm from './LoginForm/LoginForm'
 import Footer from './Footer/Footer'
+import Homepage from './Homepage/Homepage'
 import { Card } from './common'
 
 import './App.module.css'
@@ -10,13 +12,14 @@ import './App.module.css'
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header></Header>
-        <Card>
-          <LoginForm></LoginForm>
-        </Card>
-        <Footer></Footer>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header></Header>
+            <Route path="/" exact component={Homepage}></Route>
+            <Route path="/login" component={LoginForm}></Route>
+          <Footer></Footer>
+        </div>
+      </BrowserRouter>
     )
   }
 }
