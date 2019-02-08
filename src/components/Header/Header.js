69ca8a94bbc73c51c4cf22e17fb94dc9e1fc3c3b
Chris Halley <chrishalley86@gmail.com>
2019-02-08 18:15:41 +0000

    converted <Header> to class component, connected to Redux store, added conditional logic to display login/logout buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 import HeaderLink from '../common/HeaderLink/HeaderLink'
 
 import styles from './Header.module.css';
 
-const Header = () => {
-  return (
-    <header className={styles['appHeader']}>
-      <HeaderLink to="/">Home</HeaderLink>
-      <HeaderLink to="/login">Login</HeaderLink>
-    </header>
-  )
+class Header extends Component {
+
+  renderLinks() {
+    const { currentUser } = this.props
+    if (currentUser) {
+      return (
+        <Fragment>
+          <HeaderLink to="/">Home</HeaderLink>
+          <button>Log out</button>
+        </Fragment>
+      )
+    }
+    
+    return (
+      <Fragment>
+        <HeaderLink to="/">Home</HeaderLink>
+        <HeaderLink to="/login">Log in</HeaderLink>
+      </Fragment>
+    )
+  }
+
+  render() {
+    return (
+      <header className={styles['appHeader']}>
+        {this.renderLinks()}
+      </header>
+    )
+  }
 }
 
-export default Header;
+const mapStateToProps = state => {
+  return { currentUser: state.currentUser }
+}
+
+export default connect(mapStateToProps, actions)(Header);
