849ed60761ba6f94fbba33ba6063acb41d2cc6e3
Chris Halley <chrishalley86@gmail.com>
2019-02-08 15:35:58 +0000

    added redux-thunk, added as middleware in createStore() function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import ReduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import './assets/css/reset.css';
 
+const store = createStore(reducers, applyMiddleware(ReduxThunk))
+
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.querySelector('#root')
