ae84071346146b2d9dab2c3ad049e7f91e73f6be
Chris Halley <chrishalley86@gmail.com>
2019-02-17 13:52:26 +0000

    Fixed login success/fail callback by pulling api call out of Redux action and calling directly in LoginForm onSubmit callback. Moved Redux action to .then block of api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,28 @@
 import { CHANGE_AUTH } from './types';
 import api from '../apis/api'
 
-export const changeAuth = ({ email, password }) => dispatch => {
-  console.log('email: ', email)
-  console.log('password: ', password)
-  return new Promise((resolve, reject) => {
-    api.post('/auth/login', { email, password })
-      .then(res => {
-        console.log('changeAuth() action')
-        dispatch({ type: CHANGE_AUTH, payload: res.data })
-        resolve(res.data)
-      })
-      .catch(e => {
-        reject(e)
-      })
-  })
+// export const changeAuth = ({ email, password }) => dispatch => {
+//   console.log('email: ', email)
+//   console.log('password: ', password)
+//   return new Promise((resolve, reject) => {
+//     api.post('/auth/login', { email, password })
+//       .then(res => {
+//         console.log('changeAuth() action')
+//         dispatch({ type: CHANGE_AUTH, payload: res.data })
+//         resolve(res.data)
+//       })
+//       .catch(e => {
+//         reject(e)
+//       })
+//   })
 
+// }
+
+export const changeAuth = (payload) => {
+  return {
+    type: CHANGE_AUTH,
+    payload: payload
+  }
 }
 
 export const logout = () => {
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,10 +3,13 @@ import { connect } from 'react-redux';
 
 import * as actions from '../../actions';
 import { Button, Form, Toast } from '../common';
+import api from '../../apis/api'
 
 import styles from './LoginForm.module.css';
 
 class LoginForm extends Component {
+
+  state = { errorMesasge: null }
 
   loginDetails = { email: '', password: '', errorMessage: null }
 
@@ -14,18 +17,29 @@ class LoginForm extends Component {
     this.setState({ [stateProp]: value })
   }
 
-  loginSubmit(state) {
+  loginSubmit = (state) => {
     const { email, password } = state;
-    console.log('this: ', this)
-    this.props.changeAuth({ email, password })
-      .then(function(res) {
-        console.log('then this: ', this)
-        console.log('promise resolve: ', res)
+    // console.log('this: ', this)
+    api.post('/auth/login', { email, password })
+      .then(res => {
+        const user = res.data
+        this.props.changeAuth(user)
+        console.log('this: ', this)
       })
-      .catch(function(error) {
-        const { message } = error.response.data
-        console.log(this)
+      .catch(e => {
+        console.log(e)
+        console.log('this: ', this)
+        this.setState({ errorMessage: e.response.data.message })
       })
+    // this.props.changeAuth({ email, password })
+    //   .then(function(res) {
+    //     console.log('then this: ', this)
+    //     console.log('promise resolve: ', res)
+    //   })
+    //   .catch(function(error) {
+    //     const { message } = error.response.data
+    //     console.log(this)
+    //   })
   }
 
   // loginCallback(error) {
@@ -52,7 +66,7 @@ class LoginForm extends Component {
             </Fragment>
           )}
           </Form>
-        {/* <Toast type="error" content={errorMessage} /> */}
+        <Toast type="error" content={this.state.errorMessage} />
       </div>
     )
   }
