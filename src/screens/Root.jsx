ac087e0c4a659cb4ea979a985566b56ad3ba229b
Chris Halley <chris@Chriss-MacBook-Pro.local>
2019-08-16 17:13:19 +0100

    removed typos from Root.jsx

--- src/screens/Root.jsx
+++ src/screens/Root.jsx
@@ -6,13 +6,11 @@ import ProtectedRoute from '../components/ProtectedRoute/ProtectedRoute';
 import Dashboard from './Dashboard/Dashboard';
 import Public from '../screens/Public/Public';
 
-const Root = () => {
-  return (
-      <Switch>
-        <ProtectedRoute path="/dashboard" component={Dashboard}/>} />
-        <Route path="/" component={Public}/>} />
-      </Switch>
-  );
-}
+const Root = () => (
+  <Switch>
+    <ProtectedRoute path="/dashboard" component={Dashboard}/>
+    <Route path="/" component={Public}/>
+  </Switch>
+);
 
 export default withRouter(Root);
