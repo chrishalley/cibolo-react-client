5097378b88d879a39d0fd0cdd8ec5eb80c9fba4c
Chris Halley <chris@Chriss-MacBook-Pro.local>
2019-08-16 16:45:19 +0100

    cleaned up Root.jsx

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore ,applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleWare from 'redux-saga';
 import reducers from './reducers';
-import ReduxThunk from 'redux-thunk';
 
 import rootSaga from './sagas';
 
@@ -11,10 +10,10 @@ const sagaMiddleware = createSagaMiddleWare();
 const store = createStore(reducers, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(rootSaga);
 
-export default (props) => {
-  return (
-    <Provider store={store}>
-      {props.children}
-    </Provider>
-  );
-};
+export default ({
+  children
+}) => (
+  <Provider store={store}>
+    {children}
+  </Provider>
+);
