b500d842daf03728d4087d6ca355f11cee742a81
Chris Halley <chris@Chriss-MacBook-Pro.local>
2019-08-16 17:11:29 +0100

    refactored and cleaned up ProtectedRoute.jsx

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -4,20 +4,25 @@ import { connect } from 'react-redux';
 import { logout } from '../../actions';
 import { Spinner } from '../common';
 
-const ProtectedRoute = (props) => {
-  const { component: Component, currentUser, tokenExpiry, ...rest } = props;
+const ProtectedRoute = ({
+  component: Component,
+  currentUser, 
+  tokenExpiry,
+  logout,
+  ...restProps
+}) => {
+
   const [ auth, setAuth ] = useState(null) // Set initial auth value to null
 
   useEffect(() => {
     currentUserAuthenticated() // Check whether user is authenticated every time protected route is entered
-  })
+  }, [])
 
   const currentUserAuthenticated = () => {
     if ( !currentUser ) { // If there is no currentUser logged in, setAuth to false causing redirect to login
       setAuth(false)
-      // props.logout()
     } else if (tokenExpiry < new Date().getTime()) { // If token is expired, log user out and redirect to login
-      props.logout()
+      logout()
       setAuth(false)
     } else { // Otherwise, user must be authenticated
       setAuth(true)
@@ -31,21 +36,19 @@ const ProtectedRoute = (props) => {
       case false:
         return <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
       default:
-        return <Spinner></Spinner>
+        return <Spinner />
     }
   };
 
   return (
-    <Route {...rest} render={(props) => renderRoute(props)} />
+    <Route {...restProps} render={(props) => renderRoute(props)} />
   );
 
 }
 
-const mapStateToProps = state => {
-  return {
-    currentUser: state.auth.user,
-    tokenExpiry: state.auth.tokenExpiry
-  }
-}
+const mapStateToProps = ({ auth: { user, tokenExpiry }}) => ({
+  currentUser: user,
+  tokenExpiry: tokenExpiry
+})
 
 export default connect(mapStateToProps, { logout })(ProtectedRoute);
